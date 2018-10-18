16e32596f5ed9351024b221ee82b09b46b0a90bc
Rangel Flavio Resende Leite <rangelflavio@msn.com>
2018-10-18 18:37:18 -0400

    AP mode inserted

--- relayPortao/relayPortao.c
+++ relayPortao/relayPortao.c
@@ -10,6 +10,20 @@
 #include "esp8266.h"
 #include <queue.h>
 
+#include <string.h>
+#include <dhcpserver.h>
+#include <lwip/api.h>
+
+
+
+
+#define AP_SSID "ITRIADIOT"
+#define AP_PSK "itriadIot"
+#define TELNET_PORT 23
+
+
+
+
 int gpio = 0;
 int stateTo=0;
 
@@ -19,13 +33,6 @@ u_int portArray[]={16,5,4,0,2,14,12,13};//D0, D1, D2, D3, D4, D5, D6, D7 lolin e
 u_int sensorInput = 15;
 static QueueHandle_t tsqueue;
 scanTime = 100;
-
-
-/* This task uses the high level GPIO API (esp_gpio.h) to blink an LED.
- *
- */
-
-
 bool toChangePortState = false;
 
 
@@ -193,6 +200,53 @@ void monitoraSensor(void *pvParameters)
 }
 
 
+
+static void telnetTask(void *pvParameters)
+{
+    ip_addr_t first_client_ip;
+    IP4_ADDR(&first_client_ip, 172, 16, 0, 2);
+    dhcpserver_start(&first_client_ip, 4);
+
+    struct netconn *nc = netconn_new(NETCONN_TCP);
+    if (!nc)
+    {
+        printf("Status monitor: Failed to allocate socket.\r\n");
+        return;
+    }
+    netconn_bind(nc, IP_ANY_TYPE, TELNET_PORT);
+    netconn_listen(nc);
+
+    while (1)
+    {
+        struct netconn *client = NULL;
+        err_t err = netconn_accept(nc, &client);
+
+        if (err != ERR_OK)
+        {
+            if (client)
+                netconn_delete(client);
+            continue;
+        }
+
+        ip_addr_t client_addr;
+        uint16_t port_ignore;
+        netconn_peer(client, &client_addr, &port_ignore);
+
+        char buf[80];
+        snprintf(buf, sizeof(buf), "Uptime %d seconds\r\n", xTaskGetTickCount() * portTICK_PERIOD_MS / 1000);
+        netconn_write(client, buf, strlen(buf), NETCONN_COPY);
+        snprintf(buf, sizeof(buf), "Free heap %d bytes\r\n", (int) xPortGetFreeHeapSize());
+        netconn_write(client, buf, strlen(buf), NETCONN_COPY);
+        char abuf[40];
+        snprintf(buf, sizeof(buf), "Your address is %s\r\n\r\n", ipaddr_ntoa_r(&client_addr, abuf, sizeof(abuf)));
+        netconn_write(client, buf, strlen(buf), NETCONN_COPY);
+        netconn_delete(client);
+    }
+}
+
+
+
+
 void user_init(void)
 {
      uart_set_baud(0, 115200);
@@ -206,6 +260,18 @@ void user_init(void)
 
 
 
+    sdk_wifi_set_opmode(SOFTAP_MODE);
+    struct ip_info ap_ip;
+    IP4_ADDR(&ap_ip.ip, 172, 16, 0, 1);
+    IP4_ADDR(&ap_ip.gw, 0, 0, 0, 0);
+    IP4_ADDR(&ap_ip.netmask, 255, 255, 0, 0);
+    sdk_wifi_set_ip_info(1, &ap_ip);
+
+    struct sdk_softap_config ap_config = { .ssid = AP_SSID, .ssid_hidden = 0, .channel = 3, .ssid_len = strlen(AP_SSID), .authmode =
+            AUTH_WPA_WPA2_PSK, .password = AP_PSK, .max_connection = 3, .beacon_interval = 100, };
+    sdk_wifi_softap_set_config(&ap_config);
+
+    xTaskCreate(telnetTask, "telnetTask", 512, NULL, 2, NULL);
 
 
 }
